b966f3fb2415be466a94b2ee201a2f5e65706809
Hanks <zhanghan.me@gmail.com>
2017-05-31 15:56:31 +0800

    add test script

---
+++ test/unit/diagnose.js
@@ -0,0 +1,4 @@
+const path = require('path')
+const diagnose = require('../../src/diagnose')
+
+diagnose(path.resolve(__dirname, '../vue/div.vue'))
---
+++ test/vue/div.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<style scoped>
+  .box {
+    border-width: 2px;
+    border-style: solid;
+    border-color: rgb(65, 184, 131);
+    width: 250px;
+    height: 250px;
+    margin-top: 250px;
+    margin-left: 250px;
+    background-color: rgba(65, 184, 131, 0.25);
+  }
+</style>
