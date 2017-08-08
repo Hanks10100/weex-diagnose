32d0da4e2d44fbe9cab0d2b3374ca2cd300feae0
Hanks <zhanghan.me@gmail.com>
2017-08-08 12:13:27 +0800

    [refactor] generate vanilla codes

--- src/reporter/text/index.js
+++ src/reporter/text/index.js
@@ -2,20 +2,25 @@ const printLogs = require('./logs')
 const printHistory = require('./history')
 const printSummary = require('./summary')
 const { printWarnings, printErrors } = require('./warnings')
+const { writeFile } = require('../../utils')
 
 function generateVanillaCode (history) {
   let code = '// { "framework": "Vanilla" }\n\n'
   if (Array.isArray(history)) {
     history.forEach(({ module, method, args }) => {
-      code += `sendTasks(id,[{module:'${module}',method:'${method}',args:${JSON.stringify(args)}}],-1)\n`
+      if (module === 'dom' && method === 'createFinish') {
+        return
+      }
+      code += `sendTasks([{module:'${module}',method:'${method}',args:${JSON.stringify(args)}}])\n`
     })
   }
+  writeFile('./output.js', code)
   return code
 }
 
 module.exports = function textReporter (report, options = {}) {
-  // console.log(generateVanillaCode(report.history))
-  // printHistory(report.history)
+  generateVanillaCode(report.history)
+  printHistory(report.history)
   // printLogs(report.messages)
   printSummary(report.summary)
   printWarnings(report.warnings)
