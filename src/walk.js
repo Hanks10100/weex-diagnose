24a1b8062e7021c7aade33a14466ed6c90da751b
Hanks <zhanghan.me@gmail.com>
2017-09-28 15:05:55 +0800

    ignore some special path

--- src/walk.js
+++ src/walk.js
@@ -1,10 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 
+const excludePaths = [
+  /^\./,
+  /^node_modules$/
+]
+
+function isValidPath (filePath) {
+  return fs.lstatSync(filePath).isDirectory()
+    && !excludePaths.some(regex => regex.test(filePath))
+}
+
+function isValidFile (fileName) {
+  return fs.lstatSync(fileName).isFile()
+    && path.extname(fileName) === '.vue'
+}
+
 function walk (entryPath) {
   const stats = fs.lstatSync(entryPath)
   if (stats.isFile()) {
-    console.log(` => is file ${entryPath}`)
+    // console.log(` => is file ${entryPath}`)
     return [entryPath]
   } else if (stats.isDirectory()) {
     const subPaths = fs.readdirSync(entryPath)
@@ -12,9 +27,9 @@ function walk (entryPath) {
     if (subPaths.length) {
       for (let i = 0; i < subPaths.length; ++i) {
         const pathName = path.join(entryPath, subPaths[i])
-        if (fs.lstatSync(pathName).isDirectory()) {
+        if (isValidPath(pathName)) {
           results.push(...walk(pathName))
-        } else if (path.extname(pathName) === '.vue') {
+        } else if (isValidFile(pathName)) {
           results.push(pathName)
         }
       }
