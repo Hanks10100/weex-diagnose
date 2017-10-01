182a700804e298d007cc19d7e8e3a4e9b9816689
Hanks <zhanghan.me@gmail.com>
2017-10-01 17:28:29 +0800

    update deps

--- src/compiler/transformer/mfs.js
+++ src/compiler/transformer/mfs.js
@@ -3,12 +3,12 @@ const preLoadLoaders = require('./preload')
 
 const mfs = new MemoryFileSystem()
 
-preLoadLoaders(mfs, [
-  'babel-loader',
-  'css-loader',
-  'vue-style-loader',
-  'weex-loader',
-  'vue-loader'
-])
+// preLoadLoaders(mfs, [
+//   'babel-loader',
+//   'css-loader',
+//   'vue-style-loader',
+//   'weex-loader',
+//   'vue-loader'
+// ])
 
 module.exports = mfs
