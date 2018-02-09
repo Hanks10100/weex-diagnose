c4112f83ce01ab24711aacc22aaad628c02b4787
erha19 <faterrole@gmail.com>
2018-02-09 17:11:59 +0800

    Release 0.3.6

--- src/reporter/filter.js
+++ src/reporter/filter.js
@@ -3,7 +3,7 @@ const reportFilters = [
   {
     rule: /<style> must be scoped/,
     type: 'error',
-    id: 'no-use-scoped'
+    id: 'must-use-scoped'
   },
   {
     rule: /this file is using stylus, the line number in <style> will be mismatch/,
@@ -13,12 +13,12 @@ const reportFilters = [
   {
     rule: /(may not be supported)/,
     type: 'warn',
-    id: 'no-standard-style-prop'
+    id: 'no-standard-style-value'
   },
   {
     rule: /(could be removed)/,
     type: 'warn',
-    id: 'no-useless-style-prop'
+    id: 'no-useless-style-value'
   },
   {
     rule: /Weex only support single-classname selector/,
@@ -38,7 +38,7 @@ const reportFilters = [
   {
     rule: /null/,
     type: 'error',
-    id: 'script-syntax-error'
+    id: 'no-script-syntax-error'
   }
 ]
 
@@ -47,13 +47,13 @@ const messageFilters = [
     name: 'style',
     rule: /CssSyntaxError/,
     type: 'error',
-    id: 'css-syntax-error'
+    id: 'no-css-syntax-error'
   },
   {
     name: 'template',
     rule: /tag <.+> has no matching end tag/,
     type: 'error',
-    id: 'template-syntax-error'
+    id: 'no-template-syntax-error'
   }
 ]
 
